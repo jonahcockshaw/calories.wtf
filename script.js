dfef9e7a5c263b9dd679b4c3ed4dc72b2d1eb375
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2022-01-21 20:14:41 +0000

    BMR Calculator V1.0

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("bmrForm").addEventListener("submit", calcBMR);
+let weightKG = document.getElementById('weight').value;
+let heightCM = document.getElementById('height').value;
+let age = document.getElementById('age').value;
+let gender = document.getElementById('gender').value;
+let BMR;
+    
+    function calcBMR(gender, weightKG, heightCM, age) {
+    
+        // Calculate BMR
+        if (gender === 'male') {
+            let BMR = 10 * weightKG + 6.25 * heightCM - 5 * age + 5;
+        } else {
+            let BMR = 10 * weightKG + 6.25 * heightCM - 5 * age - 161;
+        }
+    
+        console.log(BMR);
+        document.getElementById("output").innerHTML = BMR;
+        return false;
+    }
